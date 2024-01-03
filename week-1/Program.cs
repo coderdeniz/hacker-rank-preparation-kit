550b6e345a8908275dfb35a4b54ad2e8116f60fe
coderdeniz <denizdumanresmi@gmail.com>
2024-01-03 14:05:27 +0300

    2 / week - 1

--- week-1/Program.cs
+++ week-1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace week_1
+﻿using System.Globalization;
+
+namespace week_1
 {
     internal class Program
     {
@@ -6,6 +8,8 @@
         {
             // PlusMinus();
             // MiniMaxSum();
+            //TimeConversion();
+            //BreakingTheRecords();
         }
 
         static void PlusMinus()
@@ -38,7 +42,6 @@
             Console.WriteLine(negative / arr.Count);
             Console.WriteLine(zero/arr.Count);
         }
-   
         static void MiniMaxSum()
         {
             List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
@@ -60,6 +63,80 @@
             }
             
             Console.Write((sum - max) + " " + (sum - min));            
+        }
+        static void TimeConversion()
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            var dt = DateTime.ParseExact(
+              s, "hh:mm:sstt", CultureInfo.InvariantCulture
+            );
+
+            string result = $"{dt:HH:mm:ss}";
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }        
+        static void BreakingTheRecords()
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+            int max = 0;
+            int min = 0;
+            int minValue = scores[0];
+            int maxValue = scores[0];
+            foreach (var item in scores)
+            {
+                if (item > maxValue)
+                {
+                    max++;
+                    maxValue = item;
+                }
+                else if (item < minValue)
+                {
+                    min++;
+                    minValue = item;
+                }
+            }
+
+            List<int> result = new List<int> { max, min };
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+        static void CamelCase()
+        {
+            var text = Console.ReadLine();
+            var textArr = text.Split(";");
+            var convertedText = textArr[2];
+
+            if (textArr[0] == "S")
+            {
+                if (textArr[1] == "M")
+                {
+                    foreach (var item in convertedText)
+                    {
+                        if (char.IsUpper(item))
+                        {
+
+                        }
+                    }
+                }
+            }
+            else if (textArr[0] == "C")
+            {
+
+            }
         }
     }
 }
